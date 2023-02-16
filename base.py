1a36d5fa5f3a4f9bbc1f800195fbba88d08b15e9
Valkam <82890199+Valkam-Git@users.noreply.github.com>
2023-02-16 11:38:50 +0100

    Add files via upload

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import numpy as np
 import tkinter as tk
 from mss import mss
-from time import time
+from time import time, sleep
 
 w = 3840
 h = 2160
@@ -20,7 +20,7 @@ root.config(bg="white")
 canvas = tk.Canvas(root, bg="white", height=h, width=w)
 canvas.pack()
 
-model_path = "best.pt"
+model_path = "modelo_yolo/best.engine"
 model = torch.hub.load("ultralytics/yolov5", "custom", path=model_path)
 model.cuda()
 model.multi_label = False
@@ -39,22 +39,8 @@ class Object:
         self.coords[2], self.coords[3] = self.coords[0] + self.width, self.coords[1] + self.height
         self.confidence = confidence
 
-    def draw(self, canvas):
 
-        canvas.create_rectangle(self.coords[0], self.coords[1], self.coords[2], self.coords[3], outline="red")
-        canvas.create_text(self.coords[2] + 5,
-                           self.coords[3] + 15,
-                           text=classes[int(self.label)],
-                           fill="red",
-                           font=("Arial", 12))
-        canvas.create_text(self.coords[2] - 5,
-                           self.coords[3] - 5,
-                           text=str(round(self.confidence, 2)),
-                           fill="red",
-                           font=("Arial", 12))
-
-
-def is_duplicate(old_list, coords, label):
+def is_duplicate(coords, label):
     coords = coords * base
     for item in old_list:
         if np.linalg.norm(coords - item.coords) < 40 and label == item.label or classes[int(
@@ -63,9 +49,9 @@ def is_duplicate(old_list, coords, label):
     return False
 
 
-def remove_duplicates(old_list):
+def remove_duplicates():
     for item in old_list:
-        if is_duplicate(old_list, item.coords, item.label):
+        if is_duplicate(item.coords, item.label):
             old_list.remove(item)
 
 
@@ -77,42 +63,39 @@ classes = [
     "emetic_russula", "bugloss", "player", "mountain_flower", "white_cap", "violet_coprinus", "namiras_rot", "stinkhorn"
 ]
 
+item_list = []
+old_list = []
+time_start = time()
 
-def main_loop():
+while True:
+    screenshot = cv.cvtColor(np.array(sct.grab(monitor)), cv.COLOR_BGR2RGB)
+    results = model(screenshot)
+    labels, cord = results.xyxyn[0][:, -1].cpu().numpy(), results.xyxyn[0][:, :-1].cpu().numpy()
+
+    old_list = item_list
     item_list = []
-    old_list = []
+
+    for i in range(len(labels)):
+        if cord[i][4] > 0.3:
+            found = False
+            for item in old_list:
+                if is_duplicate(cord[i][:-1], labels[i]):
+                    item.update(cord[i], cord[i][4])
+                    item_list.append(item)
+                    found = True
+                    break
+            if not found and cord[i][4] > 0.5:
+                item_list.append(Object(cord[i], labels[i]))
+
+    remove_duplicates()
+    if len(item_list) == 0:
+        continue
+
+    for item in item_list:
+        item.draw(canvas)
+
+    canvas.update()
+    canvas.delete("all")
+
+    print(f"FPS: {1 / (time() - time_start)}")
     time_start = time()
-
-    while True:
-        screenshot = cv.cvtColor(np.array(sct.grab(monitor)), cv.COLOR_BGR2RGB)
-
-        results = model(screenshot)
-        labels, cord = results.xyxyn[0][:, -1].cpu().numpy(), results.xyxyn[0][:, :-1].cpu().numpy()
-
-        old_list = item_list
-        item_list = []
-
-        for i in range(len(labels)):
-            if cord[i][4] > 0.3:
-                found = False
-                for item in old_list:
-                    if is_duplicate(cord[i][:-1], labels[i]):
-                        item.update(cord[i], cord[i][4])
-                        item_list.append(item)
-                        found = True
-                        break
-                if not found and cord[i][4] > 0.5:
-                    item_list.append(Object(cord[i], labels[i]))
-
-        remove_duplicates(item_list)
-        if len(item_list) == 0:
-            continue
-        for item in item_list:
-            item.draw(canvas)
-
-        # refresh the canvas
-        canvas.update()
-        canvas.delete("all")
-
-        print(f"FPS: {1 / (time() - time_start)}")
-        time_start = time()
